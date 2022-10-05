ab5be46af9d99bf2d59b94ae586b4bb80f8a2d36
bsc-com-01-19 <bsc-com-01-19@unima.ac.mw>
2022-10-06 00:24:52 +0200

    question 1 has been answered for allowing only users with admin rights to disable accounts question 2 done,updating a user role requires someone who have both admin roles and executive roles

--- src/main/java/com/spring/security/demo/user/UserController.java
+++ src/main/java/com/spring/security/demo/user/UserController.java
@@ -42,6 +42,7 @@ public class UserController {
     public ResponseEntity<UserModel> getUserDetails(Authentication authentication){
         return userService.getUser(authentication);
     }
+    @Secured({"ROLE_ADMIN", "ROLE_EXECUTIVE"})
     @PutMapping("update-role/{userName}/{role}")
     public ResponseEntity<ApiResponse> updateUserRole(@PathVariable("userName") String userName,
                                                       @PathVariable("role") String role,
